e2cad036687aeffef2f2911c485df68f8a285b00
jay-java <jayprajapati.tops@gmail.com>
2025-05-17 10:52:22 +0530

    polymorphism abstraction

---
+++ Core/src/P0018_Abstraction.java
@@ -0,0 +1,41 @@
+
+abstract class RBI{
+	public abstract void HL();//abstract
+//	public void repoRate() {
+//		System.out.println("repo rate +-4%");
+//	}
+	
+	public static void repoRate() {
+		System.out.println("repo rate +-4%");
+	}
+}
+class SBI extends RBI{
+
+	@Override
+	public void HL() {
+		// TODO Auto-generated method stub
+		System.out.println("SBI HL : 8%");
+	}
+	
+}
+class JAVA extends RBI{
+
+	@Override
+	public void HL() {
+		// TODO Auto-generated method stub
+		System.out.println("JAVA HL: 8.5%");
+	}
+	
+}
+public class P0018_Abstraction {
+	public static void main(String[] args) {
+		SBI s = new SBI();
+		s.HL();
+		SBI.repoRate();
+		
+		
+		JAVA j = new JAVA();
+		j.HL();
+		JAVA.repoRate();
+	}
+}
--- Core/src/P017_Polymorphism.java
+++ Core/src/P017_Polymorphism.java
@@ -1,7 +1,39 @@
 
+class Overload {
+	public void addition(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public void addition(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
+
+	public void addition(double a, double b) {
+		System.out.println(a + b);
+	}
+}
+
+class Parent{
+	public void calling() {
+		System.out.println("calling in parent class");
+	}
+	
+}
+class Child extends Parent{
+	public void calling() {
+		super.calling();
+		System.out.println("calling in child class");
+	}
+}
 
 public class P017_Polymorphism {
 	public static void main(String[] args) {
+		Overload o = new Overload();
+		o.addition(12,12);
+		o.addition(12, 12, 34);
+		o.addition(34.34, 546.456);
 		
+		Child c = new Child();
+		c.calling();
 	}
 }
