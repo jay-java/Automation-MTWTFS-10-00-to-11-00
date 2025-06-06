596ef4c797491210ee5f54e5807cc090748367a0
jay-java <jayprajapati.tops@gmail.com>
2025-06-06 10:56:31 +0530

    html inspect

--- Selenium_Learning/src/test/java/basic/P001_OpenBrowser.java
+++ Selenium_Learning/src/test/java/basic/P001_OpenBrowser.java
@@ -1,6 +1,8 @@
 package basic;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -8,19 +10,27 @@ public class P001_OpenBrowser {
 	public static void main(String[] args) throws InterruptedException {
 		
 		//1.Chrome
-//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
-//		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		
+		WebElement emailEle = driver.findElement(By.id("email"));
+		emailEle.sendKeys("selenium@gmail.com");
+		WebElement passwordEle = driver.findElement(By.id("pass"));
+		passwordEle.sendKeys("selenium@123");
+		
+		WebElement loginBTN = driver.findElement(By.name("login"));
+//		loginBTN.click();
+//		driver.close();
+		
+		//Edge
+//		System.setProperty("webdriver.edge.driver", "C:\\Users\\Admin\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+//		W 	driver = new EdgeDriver();
 //		driver.get("https://www.facebook.com/");
 //		driver.manage().window().maximize();
 //		Thread.sleep(3000);
 //		driver.close();
-		
-		//Edge
-		System.setProperty("webdriver.edge.driver", "C:\\Users\\Admin\\Downloads\\edgedriver_win64\\msedgedriver.exe");
-		WebDriver driver = new EdgeDriver();
-		driver.get("https://www.facebook.com/");
-		driver.manage().window().maximize();
-		Thread.sleep(3000);
-		driver.close();
 	}
 }
